82216bd626f395d5f14c29cdcfe33ec26308c377
sajidRehman12 <rehmansajid919@gmail.com>
2026-06-18 13:08:01 +0500

    Initial commit

---
+++ app/test_main.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"} 
+
+ 
+
+def test_read_main_againistsss():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "W"} 
